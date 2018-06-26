f61d7818a6a7c070ff62c6d04a854517cdea2e1c
Magesh <tkmagesh77@gmail.com>
2018-06-26 16:18:45 +0530

    state manager - with spinner - initial

---
+++ 04-state-manager/stateManager.js
@@ -0,0 +1,35 @@
+var SM = (function(){
+	var _state = undefined,
+		_listeners = [],
+		_reducer = undefined,
+		__init_action = '@@init_action';
+
+	function getState(){
+		return _state;
+	}
+
+	function subscribe(listenerFn){
+		if (typeof listenerFn === 'function')
+			_listeners.push(listenerFn);
+	}
+
+	function triggerChange(){
+		_listeners.forEach(listenerFn => listenerFn());
+	}
+
+	function dispatch(action){
+		var newState = _reducer(_state, action);
+		if (newState === _state) return;
+		_state = newState;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		_reducer = reducer;
+		_state = _reducer(_state, __init_action);
+		return { getState, subscribe, dispatch };
+	}
+
+	return { createStore };
+
+})();

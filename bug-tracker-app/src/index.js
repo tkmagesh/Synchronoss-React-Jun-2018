e3fad109c7325de65e06f3e5ceb6e0e7cec4dbbd
Magesh <tkmagesh77@gmail.com>
2018-06-27 17:22:47 +0530

    Routing using react-router-dom

--- bug-tracker-app/src/index.js
+++ bug-tracker-app/src/index.js
@@ -8,16 +8,24 @@ import { Provider } from 'react-redux';
 import BugTracker from './bugTracker';
 import Spinner from './spinner';
 import appStore from './store';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 ReactDOM.render(
-				<Provider store={appStore}>
-					<div>
-						<Spinner />
-						<hr/>
-						<BugTracker />
-					</div>
-				</Provider>
-			,
-			document.getElementById('root'));
+	<Provider store={appStore}>
+		<Router>
+			<div>
+				<Link to="/">Bug Tracker</Link>
+				<br/>
+				<Link to="/spinner">Spinner</Link>
+				<br />
+			  <Route exact path="/" component={BugTracker} />
+		      <Route path="/bugs" component={BugTracker} />
+		      <Route path="/spinner" component={Spinner} />
+		     </div>
+	    </Router>
+	</Provider>
+	, document.getElementById('root')
+);
 		
 registerServiceWorker();
